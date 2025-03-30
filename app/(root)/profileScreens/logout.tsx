25fe01d6c4dfc84897f241d27b7237dd01275772
Suren <khe4oyan@gmail.com>
2025-03-30 09:25:26 +0300

    .

--- app/(root)/profileScreens/logout.tsx
+++ app/(root)/profileScreens/logout.tsx
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const Logout = () => {
+  // TODO: show logout modal, progress bar and message when logouted
   return (
     <View>
       <Text>Logout</Text>
