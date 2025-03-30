cd0db05ad81d4152d75eae7f6e1d43ec7f87abc5
Suren <khe4oyan@gmail.com>
2025-03-30 14:27:46 +0300

    s

--- app/(root)/profileScreens/_layout.tsx
+++ app/(root)/profileScreens/_layout.tsx
@@ -4,17 +4,19 @@ import { Button } from "react-native";
 export default function ProfileScreenLayout() {
   const route = useRouter();
 
-  return <Stack screenOptions={{
-    headerShown: true,
-    headerLeft: () => <Button onPress={() => route.dismiss()} title="back" />
-  }}>
-    <Stack.Screen name="personalData" options={{ headerTintColor: "#ff4e00", title: "Personal Data" }} />
-    <Stack.Screen name="paymentsMethods" options={{ headerTintColor: "#ff4e00", title: "Payments Methods" }} />
-    <Stack.Screen name="ridesHistory" options={{ headerTintColor: "#ff4e00", title: "Rides History" }} />
-    <Stack.Screen name="settings" options={{ headerTintColor: "#ff4e00", title: "Settings" }} />
-    <Stack.Screen name="myCars" options={{ headerTintColor: "#ff4e00", title: "My Cars" }} />
-    <Stack.Screen name="security" options={{ headerTintColor: "#ff4e00", title: "Security" }} />
-    <Stack.Screen name="feedback" options={{ headerTintColor: "#ff4e00", title: "Feedback" }} />
-    <Stack.Screen name="logout" options={{ headerTintColor: "#ff4e00", title: "Logout" }} />
-  </Stack>;
+  return (
+    <Stack screenOptions={{
+      headerShown: true,
+      headerLeft: () => <Button onPress={() => route.dismiss()} title="back" />
+    }}>
+      <Stack.Screen name="personalData" options={{ headerTintColor: "#ff4e00", title: "Personal Data" }} />
+      <Stack.Screen name="paymentsMethods" options={{ headerTintColor: "#ff4e00", title: "Payments Methods" }} />
+      <Stack.Screen name="ridesHistory" options={{ headerTintColor: "#ff4e00", title: "Rides History" }} />
+      <Stack.Screen name="settings" options={{ headerTintColor: "#ff4e00", title: "Settings" }} />
+      <Stack.Screen name="myCars" options={{ headerTintColor: "#ff4e00", title: "My Cars" }} />
+      <Stack.Screen name="security" options={{ headerTintColor: "#ff4e00", title: "Security" }} />
+      <Stack.Screen name="feedback" options={{ headerTintColor: "#ff4e00", title: "Feedback" }} />
+      <Stack.Screen name="logout" options={{ headerTintColor: "#ff4e00", title: "Logout" }} />
+    </Stack>
+  );
 }
