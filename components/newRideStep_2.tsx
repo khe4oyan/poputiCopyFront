48a5d56caa827021ae15930252001b4ed61be548
Suren <khe4oyan@gmail.com>
2025-04-07 13:33:59 +0400

    .

--- components/newRideStep_2.tsx
+++ components/newRideStep_2.tsx
@@ -1,11 +1,27 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { useDispatch } from 'react-redux';
+import { setDate as setDateSlice } from '@/store/slices/newRideSlice';
 
-const NewRideStep_2 = ({ setIsNextButtonDisabled }: { setIsNextButtonDisabled: any}) => {
+const NewRideStep_2 = ({ setIsNextButtonDisabled }: { setIsNextButtonDisabled: any }) => {
+  const dispatch = useDispatch();
+
+  const onChange = (_: any, selectedDate: any) => {
+    dispatch(setDateSlice(new Date(selectedDate).getTime()));
+    setIsNextButtonDisabled(false);
+  }
+
   return (
-    <View>
-      <Text>NewRideStep_2</Text>
-    </View>
+    <DateTimePicker
+      textColor='black'
+      accentColor='#ff4e00'
+      themeVariant="light"
+      testID="dateTimePicker"
+      value={new Date()}
+      mode="datetime"
+      onChange={onChange}
+    />
   )
 }
 
