89e775aaf5df88ec3273ed1d896cc101a1f427c3
Suren <khe4oyan@gmail.com>
2025-04-17 16:52:01 +0400

    .

--- i18n.js
+++ i18n.js
@@ -2,9 +2,9 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import hy from './app/locales/hy.json';
-import ru from './app/locales/ru.json';
-import en from './app/locales/en.json';
+import hy from './locales/hy.json';
+import ru from './locales/ru.json';
+import en from './locales/en.json';
 
 i18n
   .use(initReactI18next)
